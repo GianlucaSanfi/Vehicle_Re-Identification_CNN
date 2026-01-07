ebed2a20e1d00c01180f2887b8d5eb6926ac324a
Gianluca Sanfilippo <pagiansal68@gmail.com>
2026-01-07 03:27:08 +0100

    remove unwanted files from commit

--- datasets/TEST/create_test_images.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import os
-
-#create 10 test images (useless images)
-for i in range(1, 11):
-    img = Image.new('RGB', (128, 256), color = (i*10, i*20, i*15))
-    img.save(f'datasets/TEST/images/vehicle_{i}.jpg')
-
