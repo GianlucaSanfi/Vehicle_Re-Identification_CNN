622a80b0228971dbcd91ebf67e9be82d8f83e089
Gianluca Sanfilippo <pagiansal68@gmail.com>
2026-01-12 12:01:04 +0100

    fix plot bug

--- src/logger.py
+++ src/logger.py
@@ -66,7 +66,7 @@ class TrainLogger:
             plt.xlabel("Epoch")
             plt.ylabel("Loss")
             tit = "Training & Validation Loss"
-            if attention:
+            if self.attention:
                 tit += " with attention"
             plt.title(tit)
             plt.legend()
@@ -108,3 +108,43 @@ class TrainLogger:
             plt.savefig(os.path.join(self.save_dir, name_pt))
             plt.close()
 
+        """
+        # LOSS CURVE
+        plt.figure(figsize=(8, 5))
+        plt.plot(self.history["epoch"], self.history["train_loss"], label="Train Loss")
+        plt.plot(self.history["epoch"], self.history["val_loss"], label="Validation Loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        tit = "Training & Validation Loss {dataset} {backbone}"
+        if attention:
+            tit += " with attention"
+        plt.title(tit)
+        plt.legend()
+        plt.grid(True)
+        name_pt = "loss_curve_{dataset}_{backbone}_{epochs}ep"
+        if attention:
+            name_pt += "_attention"
+        name_pt += ".png"
+        plt.savefig(os.path.join(self.save_dir, name_pt))
+        plt.close()
+
+        # RANK-1 AND mAP CURVE
+        plt.figure(figsize=(8, 5))
+        plt.plot(self.history["epoch"], self.history["rank1"], label="Rank-1")
+        plt.plot(self.history["epoch"], self.history["map"], label="mAP")
+        plt.xlabel("Epoch")
+        plt.ylabel("Score")
+        tit = "Evaluation Metrics {dataset} {backbone}"
+        if attention:
+            tit += " with attention"
+        plt.title(tit)
+        plt.legend()
+        plt.grid(True)
+        name_pt = "metrics_curve_{dataset}_{backbone}_{epochs}ep"
+        if attention:
+            name_pt += "_attention"
+        name_pt += ".png"
+        plt.savefig(os.path.join(self.save_dir, name_pt))
+        plt.close()
+
+        """
